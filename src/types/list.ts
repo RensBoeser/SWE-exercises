8b64eac3ee4c9b5d3be0e98d9dc4d1dba3a26e6c
Rens Boeser <rbo@bridgevest.com>
2019-04-12 10:32:10 +0200

    list monad now composable

--- src/types/list.ts
+++ src/types/list.ts
@@ -1,4 +1,4 @@
-import {Fun} from "./fun"
+import fun, {Fun} from "./fun"
 
 type List<a> = ({
 	kind: "some"
@@ -7,30 +7,41 @@ type List<a> = ({
 } | {
 	kind: "none"
 }) & {
-	map: <b>(this: List<a>, f: Fun<a, b>) => List<b>
+	map: <b>(this: List<a>) => Fun<Fun<a, b>, List<b>>
 	join: (this: List<List<a>>) => List<a>
-	flatmap: <b>(this: List<List<a>>, f: Fun<a, b>) => List<b>
+	flatmap: <b>(this: List<a>) => Fun<Fun<a, List<b>>, List<b>>
 }
 
 const functions = <a>() => ({
-	map<b>(this: List<a>, f: Fun<a, b>): List<b> { return listMap(this, f) },
-	join(this: List<List<a>>): List<a> { return listJoin(this) },
-	flatmap<b>(this: List<List<a>>, f: Fun<a, b>): List<b> { return listFlatmap(this, f) }
+	map<b>(this: List<a>): Fun<Fun<a, b>, List<b>> { return listMap<a, b>().apply(this) },
+	join(this: List<List<a>>): List<a> { return listJoin<a>().apply(this) },
+	flatmap<b>(this: List<a>): Fun<Fun<a, List<b>>, List<b>> { return listFlatmap<a, b>().apply(this) }
 })
 
 const none = <a>(): List<a> => ({ kind: "none", ...functions<a>() })
 const some = <a>(head: a, tail: List<a>): List<a> => ({ kind: "some", head, tail, ...functions<a>() })
 
-const listMap = <a, b>(l: List<a>, f: Fun<a, b>): List<b> =>
-	l.kind === "none"
-	? none<b>()
-	: some(f.apply(l.head), listMap(l.tail, f))
+const listMap = <a, b>(): Fun<List<a>, Fun<Fun<a, b>, List<b>>> =>
+	fun(l =>
+		fun(f =>
+			l.kind === "none"
+			? none<b>()
+			: some(f.apply(l.head), listMap<a, b>().apply(l.tail).apply(f))
+		)
+	)
 
-const listJoin = <a>(l: List<List<a>>): List<a> =>
-	l.kind === "none"
-	? none<a>()
-	: l.head.kind === "none"
-	? listJoin(l.tail)
-	: listJoin(some(some(l.head.head, l.head.tail), l.tail))
+const listJoin = <a>(): Fun<List<List<a>>, List<a>> =>
+	fun(l =>
+		l.kind === "none"
+		? none<a>()
+		: l.head.kind === "none"
+		? listJoin<a>().apply(l.tail)
+		: listJoin<a>().apply(some(some(l.head.head, l.head.tail), l.tail))
+	)
 
-const listFlatmap = <a, b>(l: List<List<a>>, f: Fun<a, b>): List<b> => listMap(listJoin(l), f)
+const listFlatmap = <a, b>(): Fun<List<a>, Fun<Fun<a, List<b>>, List<b>>> =>
+	fun(l =>
+		fun(f =>
+			l.map<List<b>>().then(listJoin()).apply(f)
+		)
+	)
